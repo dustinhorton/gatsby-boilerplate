c3d2b0952ed964f1fb03450d69351f547742154a
Dustin Horton <d@dustinhorton.com>
2019-03-12 00:57:45 -0700

    add debug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,14 @@
 exports.onCreatePage = async({ actions, page }) => {
   const { createPage } = actions
 
+  // this this infinite loops with `gatsby-mdx` included
   return new Promise((resolve, reject) => {
+    console.log('page', page)
+
+    // adding this stops the infinite loop,
+    // but then it still sits at `createPagesStatefully`
+    //if (page.component.includes('.mdx')) return
+
     if (/home/.test(page.path)) {
       page.path = '/'
     }
